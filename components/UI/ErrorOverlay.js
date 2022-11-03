dc48763da4c7f2f3d109cafc757a94aab38b8ea6
Aries Marendy <ariesmarendy@pop-os.localdomain>
2022-11-03 14:27:10 +0700

    App modified

---
+++ components/UI/ErrorOverlay.js
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Button from "./Button";
+import { GlobalStyles } from "../../constants/styles";
+
+export default function ErrorOverlay({ message, onConfirm }) {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}>An error accoured!</Text>
+      <Text style={styles.text}>{message}</Text>
+      <Button onPress={onConfirm}>Okay</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  text: {
+    color: "white",
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  message: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
---
+++ util/http.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const BACKEND_URL =
+  "https://rn-expense-tracker-7e906-default-rtdb.asia-southeast1.firebasedatabase.app";
+
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
+    expenseData
+  );
+  const id = response.data.name;
+  return id;
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      title: response.data[key].title,
+    };
+
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
+}
+
+export function updateExpense(id, expenseData) {
+  return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
+}
+
+export function deleteExpense(id) {
+  return axios.delete(BACKEND_URL + `/expenses/${id}.json`);
+}
